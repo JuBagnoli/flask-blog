e33dcc06598e74c0a98893176c8854d2f1516a92
JuBagnoli <julesbagnoli@gmail.com>
2020-03-11 10:09:21 +0000

    Adding changes

--- application/routes.py
+++ application/routes.py
@@ -1,32 +1,34 @@
-# import render_template function from the flask module
-from flask import render_template
-# import the app object from the ./application/__init__.py
-from application import app
-# define routes for / & /home, this function will be called when these are accessed
-
-
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from application.models import Posts
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='our project', blogData = blogData)
+    return render_template('home.html', title='our project', blogData = blogData)
 
 @app.route('/register')
 def register():
- return render_template('register.html', title='Register here!')
+    return render_template('register.html', title='Register here!')
 
 @app.route('/about')
 def about():
- return render_template('about.html', title='Why we do what we do')
+    return render_template('about.html', title='Why we do what we do')
+ 
+@app.route('post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            title=form.title.data,
+            content=form.content.data
+            )
+        db.session.add(postData)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+	    print(form.errors)
+    return render_template('post.html', title='Post', form=form)
